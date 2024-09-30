1484b8c3c09b8b5501802b7a1006cf6d14f797cc
OriHaguel <orihaguel@gmail.com>
2024-09-30 11:17:27 +0300

    Add about me section

--- src/cmps/AboutMe.jsx
+++ src/cmps/AboutMe.jsx
@@ -1,7 +1,17 @@
+import cv from '../assets/cv/cv.pdf'
 export function AboutMe() {
     return (
-        <div>
-            <h1>About Me</h1>
-        </div>
+        <section className="about-me-container">
+            <h2>About me</h2>
+            <section className="about-me">
+                <p>
+                    I'm Ori Haguel, a Full Stack Developer with a passion for creating innovative solutions.
+                    I'm a quick learner and a team player, always eager to take on new challenges and learn new technologies.
+                </p>
+                <a href={cv} className="download-cv-btn" download="Ori_Haguel_CV.pdf">Download CV</a>
+            </section>
+
+        </section>
+
     )
 }
