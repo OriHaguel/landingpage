d45627c841621eada7f3e73f5b9a30828cc35140
OriHaguel <orihaguel@gmail.com>
2024-10-03 17:16:35 +0300

    Add optimization

--- src/cmps/AboutMe.jsx
+++ src/cmps/AboutMe.jsx
@@ -1,17 +1,16 @@
 import cv from '../assets/cv/cv.pdf'
+
 export function AboutMe() {
     return (
         <section className="about-me-container">
-            <h2>About me</h2>
-            <section className="about-me">
+            <h1>About Ori Haguel - Full Stack Developer</h1>
+            <article className="about-me">
                 <p>
                     I'm Ori Haguel, a Full Stack Developer with a passion for creating innovative solutions.
                     I'm a quick learner and a team player, always eager to take on new challenges and learn new technologies.
                 </p>
-                <a href={cv} className="download-cv-btn" download="Ori_Haguel_CV.pdf">Download CV</a>
-            </section>
-
+                <a href={cv} className="download-cv-btn" download="Ori_Haguel_CV.pdf" aria-label="Download Ori Haguel's CV">Download CV</a>
+            </article>
         </section>
-
     )
 }
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
 import { SkillCard } from "../cmps/SkillCard";
 import { AboutMe } from '../cmps/AboutMe';
+import { useState } from "react";
 
 export function Home() {
+    const [selectedSkills, setSelectedSkills] = useState(false);
     return (
         <div className="home-page">
             <main className="home-container">
                 <AboutMe />
                 <div className="content">
+                    <h2>Proficiencies</h2>
+                    <button onClick={() => setSelectedSkills(!selectedSkills)}>Toggle Skills</button>
                     <div className="skills-list">
                         <SkillCard skill="react" />
                         <SkillCard skill="nodejs" />
