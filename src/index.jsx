4ec8cb85dc0aa0fdbe6ef6b9ec4ab190264ea008
OriHaguel <orihaguel@gmail.com>
2025-01-07 02:40:05 +0200

    comment service worker

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { BrowserRouter as Router } from 'react-router-dom'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 import { RootCmp } from './RootCmp'
 
@@ -24,4 +24,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+// serviceWorkerRegistration.register()
